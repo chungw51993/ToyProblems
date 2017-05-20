9ac3d57d8af88fae26f84782ad67f602fbc95b73
Won Jin Chung <chungw51993@gmail.com>
2017-05-20 12:26:46 -0500

    Fixed variable assignment

--- Remove Minimum/removesmallest.js
+++ Remove Minimum/removesmallest.js
@@ -1,9 +1,9 @@
 function removeSmallest(numbers) {
   let result = numbers.slice();
-  let min = Infinity;
-  let index;
+  let min = result[0];
+  let index = 0;
 
-  for (let i = 0; i < result.length; i++) {
+  for (let i = 1; i < result.length; i++) {
     if (result[i] < min) {
       min = result[i];
       index = i;
